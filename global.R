b809e2c9027f5cbdea8b39c68c15f2d513f8c64c
robertjgarciaphd@gmail.com <robertjgarciaphd@gmail.com>
2022-11-02 22:38:22 -0700

    Correcting filepaths.

--- global.R
+++ global.R
@@ -11,11 +11,11 @@ library(doParallel)
 library(rpart)
 library(rpart.plot)
 
-setwd("~/Desktop/Heart-Disease-R-Shiny")
+setwd("~/Desktop/Heart-Disease-R-Shiny/Capstone-R-Shiny-Heart-Disease")
 
 #Run this line the first time you run the file 
 #to auto-generate a file in the directory called model.rda
-#source('Preprocessing.R')
+source('Preprocessing.R')
 
 #loading the model
 model_old = readRDS("./model.rda")
